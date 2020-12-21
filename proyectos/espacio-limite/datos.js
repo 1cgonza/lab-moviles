a0541526154a639ba97334cda0900c1c950faa9f
Juan Camilo González <j.gonzalezj@uniandes.edu.co>
2020-12-20 21:44:02 -0500

    ✨ Comprimir imgs con TinyPNG

--- proyectos/espacio-limite/datos.js
+++ proyectos/espacio-limite/datos.js
@@ -1,88 +1,154 @@
 import Vector from './utilidades/Vector';
+import foto1 from './imgs/foto1.jpg';
+import foto2 from './imgs/foto2.jpg';
+import foto3 from './imgs/foto3.jpg';
+import foto4 from './imgs/foto4.jpg';
+import foto5 from './imgs/foto5.jpg';
+import foto6 from './imgs/foto6.jpg';
+import foto7 from './imgs/foto7.jpg';
+import foto8 from './imgs/foto8.jpg';
+import foto9 from './imgs/foto9.jpg';
+import foto10 from './imgs/foto10.jpg';
+import foto11 from './imgs/foto11.jpg';
+import foto12 from './imgs/foto12.jpg';
+import foto13 from './imgs/foto13.jpg';
+import foto14 from './imgs/foto14.jpg';
+import foto15 from './imgs/foto15.jpg';
 
 export default [
   {
-    vector: new Vector(216, 338),
-    distMin: 100,
+    vector: new Vector(168, 243),
+    distMin: 300,
     ele: null,
     tipo: 'texto',
-    mensaje: 'HOLA!',
+    mensaje: 'Tal vez si quedaron (o quedamos) penando',
   },
   {
-    vector: new Vector(1052, 427),
-    distMin: 100,
+    vector: new Vector(1201, 170),
+    distMin: 300,
+    ele: null,
+    tipo: 'texto',
+    mensaje:
+      'Tengo una caja con acetatos, <br> El primero recibe luz <br> Pero al último le tengo miedo. <br> Tengo una caja con acetatos, <br> El segundo es autosuficiente <br> Pero al último le tengo miedo <br> Tengo una caja con acetatos,<br>El tercero es curioso<br>Pero al último le tengo miedo<br>Tengo una caja con acetatos,<br>El cuarto se mueve a mi ritmo<br>Pero al último le tengo miedo<br>Tengo una caja con acetatos<br>Y ya no tengo miedo de quedar ciega.',
+  },
+  {
+    vector: new Vector(694, 301),
+    distMin: 300,
     ele: null,
     tipo: 'imagen',
-    url: ',,,,',
-    mensaje: '',
+    url: foto1,
   },
   {
-    vector: new Vector(1864, 207),
-    distMin: 100,
+    vector: new Vector(1501, 170),
+    distMin: 300,
     ele: null,
-    tipo: 'video',
-    url: '...',
+    tipo: 'texto',
+    mensaje:
+      'Desde la entrada de mi cuarto entiendo <br> que la autonomía de mis afectos me hace dudar de mis espacios protegidos. <br> Contradecir, refutar y transgredir son los roles que establece, <br> pero me gusta pensar que busca(o) ser atendida. <br> A veces muevo la cama de lugar <br> pero el problema escapa de lo físico. <br> Su movimiento me supera. <br> La contradicción no es con su cuerpo, es con el mío. <br> Es hora de cambiar de soporte.',
   },
   {
-    vector: new Vector(2344, 214),
-    distMin: 100,
+    vector: new Vector(2384, 206),
+    distMin: 300,
     ele: null,
-    tipo: 'sonido',
-    url: '...',
+    tipo: 'imagen',
+    url: foto2,
   },
   {
-    vector: new Vector(1648, 607),
-    distMin: 100,
+    vector: new Vector(1065, 415),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto3,
   },
   {
-    vector: new Vector(3845, 718),
-    distMin: 100,
+    vector: new Vector(2437, 485),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto4,
   },
   {
-    vector: new Vector(1568, 1488),
-    distMin: 100,
+    vector: new Vector(1016, 963),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto5,
   },
   {
-    vector: new Vector(118, 1750),
-    distMin: 100,
+    vector: new Vector(116, 1128),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto6,
   },
   {
-    vector: new Vector(3191, 1800),
-    distMin: 100,
+    vector: new Vector(2093, 1021),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto7,
   },
   {
-    vector: new Vector(2797, 1887),
-    distMin: 100,
+    vector: new Vector(2035, 1163),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto8,
   },
   {
-    vector: new Vector(100, 2179),
-    distMin: 100,
+    vector: new Vector(1786, 1219),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto9,
   },
   {
-    vector: new Vector(2168, 2456),
-    distMin: 100,
+    vector: new Vector(98, 1399),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto10,
   },
   {
-    vector: new Vector(1258, 2589),
-    distMin: 100,
+    vector: new Vector(1398, 1569),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto11,
   },
   {
-    vector: new Vector(1937, 2893),
-    distMin: 100,
+    vector: new Vector(826, 1655),
+    distMin: 300,
     ele: null,
+    tipo: 'texto',
+    mensaje: '¿hasta qué punto puede la imagen virtual satisfacer, cuando en su esencia se encuentra la carencia?',
   },
   {
-    vector: new Vector(268, 3605),
-    distMin: 100,
+    vector: new Vector(1246, 1845),
+    distMin: 300,
     ele: null,
+    tipo: 'imagen',
+    url: foto12,
+  },
+  {
+    vector: new Vector(1307, 2281),
+    distMin: 300,
+    ele: null,
+    tipo: 'imagen',
+    url: foto13,
+  },
+  {
+    vector: new Vector(204, 2286),
+    distMin: 300,
+    ele: null,
+    tipo: 'imagen',
+    url: foto14,
+  },
+  {
+    vector: new Vector(2437, 2424),
+    distMin: 300,
+    ele: null,
+    tipo: 'imagen',
+    mensaje: foto15,
   },
 ];
